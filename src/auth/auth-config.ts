ccd13d9380214eb097239a337021447492a63cce
devojeet02 <devojeet.modak@acumant.com>
2026-02-13 12:41:14 +0530

    correct credentials entered

--- src/auth/auth-config.ts
+++ src/auth/auth-config.ts
@@ -2,15 +2,15 @@ import { LogLevel } from '@azure/msal-browser';
 
 export const msalConfig = {
     auth: {
-        clientId: '5d31bed7-2372-4e43-9365-1706ff6601de', 
-        authority: 'https://login.microsoftonline.com/3f63b506-db59-465e-8d23-78c6cd72c13b/', 
-        redirectUri: '/', 
-        postLogoutRedirectUri: '/', 
-        navigateToLoginRequestUrl: false, 
+        clientId: 'e57495d0-8be6-4098-b01c-0293e5a99d44',
+        authority: 'https://login.microsoftonline.com/83a672d2-c8cc-460a-b754-8e37feb3f398/',
+        redirectUri: '/',
+        postLogoutRedirectUri: '/',
+        navigateToLoginRequestUrl: false,
     },
     cache: {
-        cacheLocation: 'sessionStorage', 
-        storeAuthStateInCookie: false, 
+        cacheLocation: 'sessionStorage',
+        storeAuthStateInCookie: false,
     },
     system: {
         loggerOptions: {
